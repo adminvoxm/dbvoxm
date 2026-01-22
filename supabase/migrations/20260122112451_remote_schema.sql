ce83d11a615b8256c0a037f94ed8085c3c094333
Samuel Gleize-Bourras <samuel@shunpo.fr>
2026-01-22 12:26:59 +0100

    DB: ajout column entity dans widget

---
+++ supabase/migrations/20260122112451_remote_schema.sql
@@ -0,0 +1,11 @@
+alter table "public"."entity" drop constraint "entity_old_ref_key";
+
+drop index if exists "public"."entity_old_ref_key";
+
+alter table "public"."widget_schema" add column "entity" uuid;
+
+alter table "public"."widget_schema" add constraint "widget_schema_entity_fkey" FOREIGN KEY (entity) REFERENCES public.entity(id) not valid;
+
+alter table "public"."widget_schema" validate constraint "widget_schema_entity_fkey";
+
+
